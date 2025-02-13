fa6728e44fe1048bf18435de57a0c873d87204b2
Dario Perlo <andresperlo5@gmail.com>
2025-02-13 13:28:24 -0300

    dom en js

---
+++ js/eventos.js
@@ -0,0 +1,65 @@
+/* Evento = Accion */
+/* const botonEnviar = document.getElementById('idBotonEnviar') */
+
+
+/* const botonEnviarFormulario = (event) =>{
+  //console.log(event)
+  alert('Boton clickeado')
+} */
+
+
+/* botonEnviar.addEventListener('click', () => {
+  alert('Boton clickeado')
+}) */
+
+/* botonEnviar.addEventListener('click', botonEnviarFormulario) */
+
+//Formulario
+/* const inputUsuario = document.getElementById('idInputUsuario')
+const inputContrasenia = document.getElementById('idInputContrasenia')
+const inputRepContrasenia = document.getElementById('idInputRepContrasenia')
+const inputCheck = document.getElementById('idCheck')
+const botonEnviarRegistro = document.getElementById('botonEnviar') */
+
+/* const inputCheck = document.getElementById('idCheck') */
+
+/* const formularioRegistro = (event) =>{
+  //event.preventDefault()
+  //value - el valor por defecto // placeholder -
+ //console.log( inputUsuario.value)
+ console.log(inputCheck.checked)
+ 
+} */
+
+/* botonEnviarRegistro.addEventListener('click', formularioRegistro) */
+
+//change - input
+
+/* const eventoInputUsuario = (ev) =>{
+console.log(ev.target.value)
+} */
+
+/* inputUsuario.addEventListener('change', eventoInputUsuario) */
+
+/* inputUsuario.addEventListener('input', eventoInputUsuario) */
+
+//clase-titulo-principal
+/* const tituloPrincipal = document.getElementById('idTituloPrincipal') 
+
+tituloPrincipal.classList.add('bg-danger', 'text-white') */
+
+/* tituloPrincipal.classList.remove('clase-titulo-principal') */
+
+
+//-----------------------------------------------
+
+//Agrego un elemento desde JS al HTML
+/* const tituloPrincipal = document.getElementById('idTituloPrincipal') 
+
+
+const parrafo = document.createElement('p')
+parrafo.innerText = 'Soy un parrafo desde JS'
+//parrafo.textContent = 'Soy un parrafo desde JS'
+//parrafo.innerHTML
+console.log(parrafo)
+tituloPrincipal.appendChild(parrafo) */
